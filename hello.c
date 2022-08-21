7c6c6aeaa8104bb7e5c8c706f0d04199b1ff2c5f
Carter Fleshman <carter.fleshman@huskers.unl.edu>
2022-08-21 23:12:09 +0000

    fixed standard libary bug

--- hello.c
+++ hello.c
@@ -1,11 +1,14 @@
 /**
  * Basic Hello World Program
  */
+#include<stdlib.h>
+#include<stdio.h>
+
 int main(int argc, char **argv) {
 
     printf("Hello, I am Carter Fleshman\n");
     printf("Major is Mechanical Engineering\n");
 
     return 0;
-    
+
 }
