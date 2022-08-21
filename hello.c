360b1f1a4ab4e03a42eac01e36a6703002651644
Carter Fleshman <carter.fleshman@huskers.unl.edu>
2022-08-21 23:16:17 +0000

    added test line

--- hello.c
+++ hello.c
@@ -8,6 +8,7 @@ int main(int argc, char **argv) {
 
     printf("Hello, I am Carter Fleshman\n");
     printf("Major is Mechanical Engineering\n");
+    printf("What happened?\n");
 
     return 0;
 
