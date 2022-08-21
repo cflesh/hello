67ee1c64c1a6ae2f135e0ab86bb43c3791ce9e6e
Carter Fleshman <carter.fleshman@huskers.unl.edu>
2022-08-21 22:57:01 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+int main(int argc, char **argv) {
+
+    printf("Hello, I am Carter Fleshman\n");
+    printf("Major is Mechanical Engineering\n");
+
+    return 0;
+    
+}
